cf3e8bffaaa6b1811345612dfac91529e8fca99f
Jeff Chen <zen00326598@gmail.com>
2024-02-19 23:14:25 +0800

    config:(serilog)加入serilog log

--- Program.cs
+++ Program.cs
@@ -2,45 +2,71 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
 using tongDe.Data;
 using Vite.AspNetCore.Extensions;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
+Log.Logger = new LoggerConfiguration()
+   .ReadFrom.Configuration(builder.Configuration)
+   .CreateLogger();
 
-builder.Services.AddViteServices();
+Log.Information("Starting web host");
 
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
-
-builder.Services.AddViteServices(options =>
+try
 {
-    options.Server.AutoRun = true;
-});
+    builder.Services.AddControllersWithViews();
 
-var app = builder.Build();
-if (!app.Environment.IsDevelopment())
-{
-    var webRootProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "wwwroot"));
-    var distProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "dist"));
-    var compositeProvider = new CompositeFileProvider(webRootProvider, distProvider);
-    app.Environment.WebRootFileProvider = compositeProvider;
-    app.Environment.WebRootPath = distProvider.Root;
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
+    builder.Services.AddViteServices();
+
+    builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
+
+    builder.Services.AddViteServices(options =>
+    {
+        options.Server.AutoRun = true;
+    });
+
+    builder.Host.UseSerilog();
+
+    var app = builder.Build();
+    if (!app.Environment.IsDevelopment())
+    {
+        var webRootProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "wwwroot"));
+        var distProvider = new PhysicalFileProvider(Path.Combine(builder.Environment.ContentRootPath, "dist"));
+        var compositeProvider = new CompositeFileProvider(webRootProvider, distProvider);
+        app.Environment.WebRootFileProvider = compositeProvider;
+        app.Environment.WebRootPath = distProvider.Root;
+        app.UseExceptionHandler("/Home/Error");
+        app.UseHsts();
+    }
+    else
+    {
+        app.UseViteDevelopmentServer(true);
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseSerilogRequestLogging();
+    
+    app.UseStaticFiles();
+
+    app.UseRouting();
+
+    app.UseAuthorization();
+
+    app.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+
+    app.Run();
+
+    return 0;
 }
-else
+catch (Exception ex)
 {
-    app.UseViteDevelopmentServer(true);
+    Log.Fatal(ex, "Host terminated unexpectedly");
+    return 1;
 }
-
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
-app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-app.Run();
+finally
+{
+    Log.CloseAndFlush();
+}
